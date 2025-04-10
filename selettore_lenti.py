951027dcbbfee5ff69d53304284428c06319f7c9
Ostrica391 <alti94@hotmail.it>
2025-04-10 11:21:26 +0200

    Update selettore_lenti.py

--- selettore_lenti.py
+++ selettore_lenti.py
@@ -124,9 +124,10 @@ with open("totalsag.png", "rb") as img_file:
     encoded = base64.b64encode(img_file.read()).decode()
 
 st.markdown(f"""
-    <div style='text-align: center; margin-top: 30px;'>
-        <img src='data:image/png;base64,{encoded}' style='width: 500px; border-radius: 15px; box-shadow: 0 4px 10px rgba(0,0,0,0.2);'>
+    <div style='margin-top: 30px;'>
+        <img src='data:image/png;base64,{encoded}' style='width: 500px; margin-left: 80px; border-radius: 15px; box-shadow: 0 4px 10px rgba(0,0,0,0.2);'>
     </div>
 """, unsafe_allow_html=True)
+
 
 
